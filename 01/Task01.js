3bbee0c98dd8d1627eff581656504986e7603e61
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-02-06 11:00:30 +0100

    Finish task 1

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+
+import Home from "../src/components/Home";
+import Contact from "../src/components/Contact";
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+  const nav = (
+    <ul>
+      <li>
+        <NavLink to="/task01/home">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/task01/contact">Contact</NavLink>
+      </li>
+    </ul>
+  );
+  return (
+    <Router>
+      <h1>Task01</h1>
+      {nav}
+      <Route path="/task01/home" component={Home} />
+      <Route path="/task01/contact" component={Contact} />
+    </Router>
+  );
+};
 
 export default Task01;
-
