fafd91e49af2f4ea3c4b49be3c95b10dddcbf382
AnnaBuller <aniabela@wp.pl>
2021-02-19 14:43:54 +0100

    solve 03 task

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import { v4 as uuid } from 'uuid';
+import {Route, HashRouter as Router} from 'react-router-dom';
+import Shop from '../src/components/Shop'
 import products from './../src/products.json';
+
+const categories = getCategories()
+
+function getCategories() {
+    const allCategories = products.map(prod => prod.category)
+    const categoriesNotDuplicated = new Set(allCategories);
+    const categories = [];
+    categoriesNotDuplicated.forEach(category => {
+        const categoryData =  {name: category.toLowerCase(), key: uuid()};
+        categories.push(categoryData)
+    });
+    return categories
+}
+
+function filterByCategory(category) {
+    return products.filter(prod => prod.category.toLowerCase() === category)
+}
+
+function createRoutesWithCategories() {
+    return categories.map(({name, key}) => {
+        return <Route key={key} path={`/task03/${name}`}><Shop products={filterByCategory(name)}/></Route>
+    })
+}
 
 const Task03 = () => {
     return (
-        <h1>Task03</h1>
+        <Router>
+            <h1>Task03</h1>
+            <Route exact path='/task03/'><Shop products={products}/></Route>
+            {createRoutesWithCategories()}
+        </Router>
     );
 }
 
