5da6adad4d59add4201ee69aac9922515084faf5
AnnaBuller <aniabela@wp.pl>
2021-02-19 14:02:56 +0100

    solve 02 task

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Route, HashRouter as Router} from 'react-router-dom';
+import products from '../src/products.json';
+import Product from '../src/components/Product'
 
 const Task02 = () => {
+    const routs = products.map(product => {
+        const pathname = `/task02/product-${product.id}`;
+        return <Route key={product.id} path={pathname}><Product {...product}/></Route>
+    })
+
     return (
-        <h1>Task02</h1>
+        <Router>
+            <h1>Task02</h1>
+            {routs}
+        </Router>
     );
 }
 
