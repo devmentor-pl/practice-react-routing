803fdf98352e50f30607c30be3f244da4a930eef
AnnaBuller <aniabela@wp.pl>
2021-02-21 10:46:47 +0100

    add links to products

--- 03/Task03.js
+++ 03/Task03.js
@@ -6,54 +6,64 @@ import products from './../src/products.json';
 
 
 const Task03 = () => {
-const categories = getCategories()
-const activeStyle = {color: '#333'}
+	const categories = getCategories();
+	const activeStyle = { color: '#333' };
 
-function getCategories() {
-    const allCategories = products.map(prod => prod.category)
-    const categoriesNotDuplicated = new Set(allCategories);
-    const categories = [];
-    categoriesNotDuplicated.forEach(category => {
-        const categoryData =  {name: category.toLowerCase(), key: uuid()};
-        categories.push(categoryData)
-    });
-    return categories
-}
-
-function filterByCategory(category) {
-    return products.filter(prod => prod.category.toLowerCase() === category)
-}
-function createNavLinks() {
-	return categories.map(({ name, key }) => {
-		return (
-			<li key={key}>
-				<NavLink activeStyle={activeStyle}  to={`/task03/${name}`}>
-					{name}
-				</NavLink>
-			</li>
-		);
+	const productsWithLinks = products.map((product) => {
+		return {
+			...product,
+			link: `#/task02/product-${product.id}`,
+		};
 	});
-}
-function createRoutesWithCategories() {
-    return categories.map(({name, key}) => {
-        return <Route key={key} path={`/task03/${name}`}><Shop products={filterByCategory(name)}/></Route>
-    })
-}
 
+	function getCategories() {
+		const allCategories = products.map((prod) => prod.category);
+		const categoriesNotDuplicated = new Set(allCategories);
+		const categories = [];
+		categoriesNotDuplicated.forEach((category) => {
+			const categoryData = { name: category.toLowerCase(), key: uuid() };
+			categories.push(categoryData);
+		});
+		return categories;
+	}
 
+	function filterByCategory(category) {
+		return productsWithLinks.filter(
+			(prod) => prod.category.toLowerCase() === category
+		);
+	}
+	function createNavLinks() {
+		return categories.map(({ name, key }) => {
+			return (
+				<li key={key}>
+					<NavLink activeStyle={activeStyle} to={`/task03/${name}`}>
+						{name}
+					</NavLink>
+				</li>
+			);
+		});
+	}
+	function createRoutesWithCategories() {
+		return categories.map(({ name, key }) => {
+			return (
+				<Route key={key} path={`/task03/${name}`}>
+					<Shop products={filterByCategory(name)} />
+				</Route>
+			);
+		});
+	}
 
-
-    return (
+	return (
 		<Router>
 			<h1>Task03</h1>
 			<nav>
 				<ul>{createNavLinks()}</ul>
 			</nav>
 			<Route exact path="/task03/">
-				<Shop products={products} />
+				<Shop products={productsWithLinks} />
 			</Route>
 			{createRoutesWithCategories()}
-            <Link to='/task03'>{'<<< ' + 'Go to main page'}</Link>
+			<Link to="/task03">{'<<< ' + 'Go to main page'}</Link>
 		</Router>
 	);
 }
