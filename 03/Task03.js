5a1b299f6594f283486def7beb616a5f4c6ad03e
AnnaBuller <aniabela@wp.pl>
2021-02-21 10:36:20 +0100

    add navigation to 03 task

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { v4 as uuid } from 'uuid';
-import {Route, HashRouter as Router} from 'react-router-dom';
+import { Route, NavLink, Link, HashRouter as Router } from 'react-router-dom';
 import Shop from '../src/components/Shop'
 import products from './../src/products.json';
 
+
+const Task03 = () => {
 const categories = getCategories()
+const activeStyle = {color: '#333'}
 
 function getCategories() {
     const allCategories = products.map(prod => prod.category)
@@ -20,21 +23,39 @@ function getCategories() {
 function filterByCategory(category) {
     return products.filter(prod => prod.category.toLowerCase() === category)
 }
-
+function createNavLinks() {
+	return categories.map(({ name, key }) => {
+		return (
+			<li key={key}>
+				<NavLink activeStyle={activeStyle}  to={`/task03/${name}`}>
+					{name}
+				</NavLink>
+			</li>
+		);
+	});
+}
 function createRoutesWithCategories() {
     return categories.map(({name, key}) => {
         return <Route key={key} path={`/task03/${name}`}><Shop products={filterByCategory(name)}/></Route>
     })
 }
 
-const Task03 = () => {
+
+
+
     return (
-        <Router>
-            <h1>Task03</h1>
-            <Route exact path='/task03/'><Shop products={products}/></Route>
-            {createRoutesWithCategories()}
-        </Router>
-    );
+		<Router>
+			<h1>Task03</h1>
+			<nav>
+				<ul>{createNavLinks()}</ul>
+			</nav>
+			<Route exact path="/task03/">
+				<Shop products={products} />
+			</Route>
+			{createRoutesWithCategories()}
+            <Link to='/task03'>{'<<< ' + 'Go to main page'}</Link>
+		</Router>
+	);
 }
 
 export default Task03;
