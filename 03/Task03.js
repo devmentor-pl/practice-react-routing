e44fe27fe48d6b9731a6eb7b8d51c2efd508dcae
AnnaBuller <aniabela@wp.pl>
2021-02-23 14:32:23 +0100

    change multiple routes to one with parameter

--- 03/Task03.js
+++ 03/Task03.js
@@ -4,17 +4,27 @@ import { Route, NavLink, Link} from 'react-router-dom';
 import Shop from '../src/components/Shop'
 import products from './../src/products.json';
 
-
-const Task03 = () => {
-	const categories = getCategories();
-	const activeStyle = { color: '#333' };
-
-	const productsWithLinks = products.map((product) => {
+const productsWithLinks = products.map((product) => {
 		return {
 			...product,
 			link: `#/task02/product-${product.id}`,
 		};
 	});
+	
+function filterByCategory(category) {
+	return productsWithLinks.filter(
+		(prod) => prod.category.toLowerCase() === category
+	);
+}
+
+const ShopWithCategory = ({ match }) => {
+	const { category } = match.params;
+	return <Shop products={filterByCategory(category)} />;
+};
+
+const Task03 = () => {
+	const categories = getCategories();
+	const activeStyle = { color: '#333' };
 
 	function getCategories() {
 		const allCategories = products.map((prod) => prod.category);
@@ -27,11 +37,6 @@ const Task03 = () => {
 		return categories;
 	}
 
-	function filterByCategory(category) {
-		return productsWithLinks.filter(
-			(prod) => prod.category.toLowerCase() === category
-		);
-	}
 	function createNavLinks() {
 		return categories.map(({ name, key }) => {
 			return (
@@ -40,15 +45,6 @@ const Task03 = () => {
 						{name}
 					</NavLink>
 				</li>
-			);
-		});
-	}
-	function createRoutesWithCategories() {
-		return categories.map(({ name, key }) => {
-			return (
-				<Route key={key} path={`/task03/${name}`}>
-					<Shop products={filterByCategory(name)} />
-				</Route>
 			);
 		});
 	}
@@ -62,11 +58,11 @@ const Task03 = () => {
 			<Route exact path="/task03/">
 				<Shop products={productsWithLinks} />
 			</Route>
-			{createRoutesWithCategories()}
+			<Route path="/task03/:category" component={ShopWithCategory} />
 			<Link to="/task03">{'<<< ' + 'Go to main page'}</Link>
 		</>
 	);
-}
+};
 
 export default Task03;
 
