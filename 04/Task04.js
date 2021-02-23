5b239647f3442176af9b4b28186e59f8357a2d52
AnnaBuller <aniabela@wp.pl>
2021-02-23 14:43:40 +0100

    change multiple routes to one with parameter

--- 04/Task04.js
+++ 04/Task04.js
@@ -3,6 +3,11 @@ import {useHistory, Route} from 'react-router-dom';
 import Shop from '../src/components/Shop'
 import products from './../src/products.json';
 
+const ShopSorted = ({ match }) => {
+	const { sortParam } = match.params;
+	return <Shop products={sortingFuncs[sortParam]()} />;
+};
+
 const sortingFuncs = {
     sortByPriceAsc: () => {return [...products].sort((a, b) => a.price - b.price)},
     sortByPriceDesc: () => {return [...products].sort((a, b) => b.price - a.price)},
@@ -21,9 +26,9 @@ const sortingFuncs = {
 }
 
 const Task04 = () => {
-    const mainPath = `/task04`;
-    const history = useHistory();
-    const options = [
+	const mainPath = `/task04`;
+	const history = useHistory();
+	const options = [
 		{ path: mainPath, innerText: '-' },
 		{ path: 'sortByPriceAsc', innerText: 'price ascending' },
 		{ path: 'sortByPriceDesc', innerText: 'price descending' },
@@ -32,42 +37,39 @@ const Task04 = () => {
 		{ path: 'sortById', innerText: 'product ID' },
 	];
 
-    function renderOptions() {
-        return options.map(({path, innerText}) => <option key={path} value={path}>{innerText}</option>)
-    }
+	function renderOptions() {
+		return options.map(({ path, innerText }) => (
+			<option key={path} value={path}>
+				{innerText}
+			</option>
+		));
+	}
 
-    function renderRoutes() {
-        return options.map(({path}) => {
-            if(path === mainPath) {
-                return <Route exact key={path} path={path}><Shop products={products}/></Route>;
-            } else {
-                const innerPath = `${mainPath}/${path}`;
-                return <Route key={path} path={innerPath}><Shop products={sortingFuncs[path]()}/></Route>;
-            }
-        })
-    }
+	function handleChange({ target: { value } }) {
+		if (value === mainPath) {
+			history.push(mainPath);
+		} else {
+			const path = `${mainPath}/${value}`;
+			history.push(path);
+		}
+	}
 
-    function handleChange({target: {value}}) {
-        if(value === mainPath) {
-            history.push(mainPath)
-        } else {
-            const path = `${mainPath}/${value}`;
-            history.push(path)
-        }
-    }
-
-    return (
-        <>
-            <h1>Task04</h1>
-            <label htmlFor="sort">Sort by: 
-                <select name='sort' onChange={e => handleChange(e)}>
-                    {renderOptions()}
-                </select>
-            </label>
-            {renderRoutes()}
-
-        </>
-    );
+	return (
+		<>
+			<h1>Task04</h1>
+			<label htmlFor="sort">
+				Sort by:
+				<select name="sort" onChange={(e) => handleChange(e)}>
+					{renderOptions()}
+				</select>
+			</label>
+			<Route exact path={mainPath}>
+				<Shop products={products} />
+			</Route>
+			;
+			<Route path={`${mainPath}/:sortParam`} component={ShopSorted} />
+		</>
+	);
 }
 
 export default Task04;
