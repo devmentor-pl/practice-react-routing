4742b265b8b8eff91a8b183773e3cf251ba009b0
Martyna Kiljan <kmartynak@icloud.com>
2021-02-27 14:52:00 +0100

    01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {HashRouter as Router,Route}from 'react-router-dom';
+import Contact from '../src/components/Contact'
+import Home from '../src/components/Home'
+import { Link } from 'react-router-dom';
 
 const Task01 = () => {
+
     return (
+        <Router>
         <h1>Task01</h1>
+        <ul>
+             <li><Link to="/task01/home">Home</Link></li>
+             <li><Link to="/task01/contact">Contact</Link></li> 
+        </ul> 
+             <Route
+                 path="/task01/home"
+                 component={Home}
+                 exact 
+              />
+              <Route
+                path="/task01/contact"
+                component={Contact} 
+              />
+        </Router>
     );
 }
 
