3423fb18acc2c2f4cf1043309cd8cec81ecc7495
Martyna Kiljan <kmartynak@icloud.com>
2021-03-04 23:22:26 +0100

    attempt to fix

--- 05/Task05.js
+++ 05/Task05.js
@@ -10,32 +10,37 @@ const Task05 = () => {
   const history = useHistory();
   const home = `/task05`;
 
-    const sortedProducts = ({ match }) => {
-      const { minPrice, maxPrice, searchTerm } = match.params;
-      return sortingFun(searchTerm, minPrice, maxPrice)
-   
-    };
+      const sortedProducts = ({ match }) => {
+        const { minPrice, maxPrice, searchTerm } = match.params;
+        return sortingFun(minPrice, maxPrice, searchTerm)
+    
+      };
 
-      const sortingFun = (searchTerm, minPrice,maxPrice) => {
-        const searchedWord = searchTerm.toLowerCase();
-        const sortedProduct = products.filter(({name}) => name.toLowerCase().includes(searchedWord)) && products.filter(({price}) => price >= minPrice && price <= maxPrice)
+      const sortingFun = (minPrice = '0',  maxPrice = '100', searchTerm = '') => {
+       const sortedAllProducts  = products.filter(({price, name}) => {
+         let sortedProductByPrice = price >= minPrice && price <= maxPrice
 
+          if(sortedProductByPrice && searchTerm.length !== 0 ) {
 
-        // if(sortedProduct.length === 0) {
-        //     alert('no results found :(')
-        //   } else {    <== JAK POWINNM TU NAPISAC WARUNEK ? CO ROBIĘ NIE TAK?
-        //       return <Shop products={sortedProduct} />
-        //   }
-          return <Shop products={sortedProduct} />;
+            const searchedWord = searchTerm.toLowerCase();
+            const sortedProductByWord = name.toLowerCase().includes(searchedWord)
+       
+            if(sortedProductByWord) {
+             return name;
+            }
+
+          }      
+          
+       })
+       return <Shop products={sortedAllProducts} />;
       }
-
-
+   
 
   
       const [values, setValues] = useState({
-      minPrice: '',
-      maxPrice: '',
-      searchTerm: '',
+        minPrice: '',
+        maxPrice: '',
+        searchTerm: '',
       })
 
       const handleChange = e => {
@@ -47,64 +52,65 @@ const Task05 = () => {
       }
 
       const handleSubmit = e => {
-        const {minPice, maxPice, searchTerm} = values;
+        const {minPrice, maxPrice, searchTerm} = values;
         e.preventDefault()
-        if(!minPice && !maxPice && !searchTerm) {
-          alert('fill in all the data!')
-        } else {
-          history.push(`${home}/${values.minPrice}/${values.maxPrice}/${values.searchTerm}`);
-          console.log(history.location)
+    
+          history.push(`${home}/${minPrice}/${maxPrice}/${searchTerm}`);
           setValues({
             minPrice: '',
             maxPrice: '',
             searchTerm: '',
           })
-        }
+        
 
       }
   
+  
 
     return (
-        <>
-        <h1>Task05</h1>
-        <form onSubmit={handleSubmit}> 
-          <p>Enter what you are looking for:</p>
-          <input
-          type="text"
-          placeholder="Search"
-          id="searchTerm"
-          name="searchTerm"
-          value={values.searchTerm}
+      <>
+      <h1>Task05</h1>
+      <form onSubmit={handleSubmit}> 
+        <p>Enter what you are looking for:</p>
+        <input
+        type="text"
+        placeholder="Search"
+        id="searchTerm"
+        name="searchTerm"
+        value={values.searchTerm}
+        onChange={handleChange}
+      />
+      <p>Enter the price range:</p>
+      <input placeholder="min"
+          type="number"
+          placeholder="min"
+          id="minPrice"
+          name="minPrice"
+          value={values.minPrice}
           onChange={handleChange}
-         />
-         <p>Enter the price range:</p>
-         <input placeholder="min"
-            type="number"
-            placeholder="min"
-            id="minPrice"
-            name="minPrice"
-            value={values.minPrice}
-            onChange={handleChange}
-          />
-         <input placeholder="max"
-             type="number"
-             placeholder="max"
-             id="maxPrice"
-             name="maxPrice"
-             value={values.maxPrice}
-             onChange={handleChange}
-          />
-             <button type="submit">search!</button>
-        </form>
+        />
+      <input placeholder="max"
+          type="number"
+          placeholder="max"
+          id="maxPrice"
+          name="maxPrice"
+          value={values.maxPrice}
+          onChange={handleChange}
+        />
+          <button type="submit">search!</button>
+      </form>
 
-        <Route exact path={home}>
-           <Shop products={products} />
-        </Route>
+      <Route exact path={home}>
+        <Shop products={products} />
+      </Route>
 
-        <Route path={`${home}/:minPrice/:maxPrice/:searchTerm`} component={sortedProducts} />
-        </>
-    );
+      <Route path={`${home}/:minPrice/:maxPrice/:searchTerm`} component={sortedProducts} />
+      <Route path={`${home}/:minPrice/:maxPrice`} component={sortedProducts} />
+      <Route path={`${home}/:searchTerm`} component={sortedProducts} />
+
+      </>
+  );
+  
 }
 
 export default Task05;
-
