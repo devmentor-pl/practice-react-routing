016b742cea7759f2ff2783b7867d2c5211a743ef
Kowacka Malgorzata <kowackag@gmail.com>
2022-03-06 12:34:20 +0100

    Delete Router from Task01

--- 01/Task01.js
+++ 01/Task01.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home  from './../src/components/Home';
 import Contact from './../src/components/Contact';
-import { HashRouter as Router } from 'react-router-dom';
+
 
 const Task01 = () => {
    
     return (
-        <Router>
+        <>
             <h1>Task01</h1>
             <Switch>
                 <Route exact path='/task01/home'><Home/></Route>
                 <Route exact path='/task01/contact'><Contact/></Route>
             </Switch>
-        </Router>
+        </>       
     );
 }
 
--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import products from './../src/products.json'
 const Task02 = () => {
     return (
-        <h1>Task02</h1>
+        <>
+            <h1>Task02</h1>
+           
+        </>
     );
 }
 
