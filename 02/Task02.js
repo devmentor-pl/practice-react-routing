8686085dd58028adcd04274e369a07c5fe2d72c9
Kowacka Malgorzata <kowackag@gmail.com>
2022-03-06 12:58:37 +0100

    Task02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import products from './../src/products.json'
+import { Switch, Route, useParams, Redirect } from 'react-router-dom';
+import ProductComponent from './../src/components/Product';
+
+const Product = () => {
+    const {id} = useParams();
+    const [product=null] = products.filter(item=>item.id===parseInt(id))
+    
+    if (product===null) {
+        return <Redirect to='/404.html'/>
+    }
+    
+    const {id:idProd} = product
+    
+    // zastanawiałąm się czy powinnam umieszczać w liscie skoro wyswietlany jest jeden produkt a nie cala lista ?
+    return(
+        <ul>    
+            <li key={idProd}><ProductComponent {...product}/></li>
+        </ul>
+    )
+}
+
 const Task02 = () => {
+
     return (
         <>
             <h1>Task02</h1>
-           
+            <Switch>
+                <Route exact path='/task02/:product-:id'><Product/></Route>
+            </Switch>
         </>
     );
 }
