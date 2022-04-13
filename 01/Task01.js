3df42650ecddacadee17d5186b1124733d58c377
Rafał Kazik <kazik.raf@gmail.com>
2022-04-13 14:34:42 +0200

    1st task

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Home from '../src/components/Home';
+import Contact from '../src/components/Contact';
+import { Route } from 'react-router-dom';
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+  return (
+    <>
+      <h1>Task01</h1>
+      <Route path='/task01/home' component={Home} />
+      <Route path='/task01/contact' component={Contact} />
+    </>
+  );
+};
 
 export default Task01;
-
