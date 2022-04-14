fb432ccc5c36e0bd45daab0a0204f8a8282ec0f0
Rafał Kazik <kazik.raf@gmail.com>
2022-04-14 14:20:50 +0200

    3rd task

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import Shop from '../src/components/Shop';
 import products from './../src/products.json';
+import NotFound from '../src/components/NotFound';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const Task03 = () => {
-    return (
-        <h1>Task03</h1>
-    );
-}
+  return (
+    <>
+      <h1>Task03</h1>
+      <Switch>
+        <Route exact path='/task03/'>
+          <Shop products={products} />
+        </Route>
+        <Route path='/task03/javascript'>
+          <Shop
+            products={products.filter((item) => item.category === 'JavaScript')}
+          />
+        </Route>
+        <Route path='/task03/react'>
+          <Shop
+            products={products.filter((item) => item.category === 'React')}
+          />
+        </Route>
+        <Route path='/404.html'>
+          <NotFound />
+        </Route>
+        <Route>
+          <Redirect to='/404.html' />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default Task03;
-
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Product = ({name, description, price, link = null}) => {
+const Product = ({ name, description, price, category, link = null }) => {
+  const productTitle = link === null ? name : <a href={link}>{name}</a>;
 
-    const productTitle = link === null ? name : <a href={ link }>{name}</a>;
-    
-    return (
-        <article>
-            <h3>{productTitle}</h3>
-            <h4>{price}</h4>
-            <p>{description}</p>
-        </article>
-    );
-}
+  return (
+    <article>
+      <h3>{productTitle}</h3>
+      <h4>{price}</h4>
+      <p>{description}</p>
+      <p>kategoria: {category}</p>
+    </article>
+  );
+};
 
 export default Product;
