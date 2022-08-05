eca76a935f3cf255649a0a71126db4002bbb32f5
Anna <krolikowska.ann@gmail.com>
2022-08-05 17:19:28 +0200

    04 added select list

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useHistory, Route,Switch } from 'react-router-dom';
+import Shop from '../src/components/Shop';
+import products from '../src/products.json'
 
 const Task04 = () => {
+
+    const history = useHistory()
+
+    const handleChange = e => {
+       history.push(`/task04/price-${e.target.value}`)
+    }
+
+    const currentSort = history.location.pathname.replace("/task04/price-","")
+
     return (
+        <>
         <h1>Task04</h1>
+        <select defaultValue={currentSort} onChange={handleChange}>
+            <option value='desc'>cena malejąco</option>
+            <option value='asc'>cena rosnąco</option>
+        </select>
+        <Switch>
+            <Route path="/task04/price-asc">
+                <Shop products={[...products].sort((a,b)=>a.price-b.price) }/>
+            </Route>
+            <Route path="/task04/price-desc">
+                <Shop products={[...products].sort((a,b)=>b.price-a.price)} />
+            </Route>
+        </Switch>
+        </>
     );
 }
 
 export default Task04;
-
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Product from './Product';
 
 const Shop = ({products}) => {
+
     return (
         <section>
             { products.map(p => <Product {...p} key={p.id}/>)}
