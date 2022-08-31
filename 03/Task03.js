43d10fda5d48768da2bcb1f882ce822169e5337b
Jakub Parol <kubciop33@gmail.com>
2022-08-31 21:43:16 +0200

    add exercises

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 
-import products from './../src/products.json';
+import products from "./../src/products.json";
+
+import Shop from "../src/components/Shop";
+import NotFound from "../src/components/NotFound";
 
 const Task03 = () => {
-    return (
-        <h1>Task03</h1>
-    );
-}
+  const history = useHistory();
+
+  const currentCategory = history.location.pathname.replace("/task03/", "");
+
+  const getItemsFromCategory = (category) => {
+    return products.filter((product) => product.category === category);
+  };
+
+  const selectCategory = (e) => {
+    const selectedCategory = e.target.value;
+
+    history.push(`/task03/${selectedCategory}`);
+
+    if (selectedCategory === "all") history.push("/task03");
+  };
+
+  return (
+    <>
+      <h1>Task03</h1>
+
+      <label htmlFor="category">Wybierz kategorię</label>
+      <select //
+        id="category"
+        defaultValue={currentCategory !== "/task03" ? currentCategory : "all"}
+        onChange={selectCategory}
+      >
+        <option value="all">Pokaż wszystko</option>
+        <option value="javascript">JavaScript</option>
+        <option value="react">React</option>
+      </select>
+
+      <Switch>
+        <Route exact path="/task03">
+          <Shop products={products} />
+        </Route>
+
+        <Route path="/task03/javascript">
+          <Shop products={getItemsFromCategory("JavaScript")} />
+        </Route>
+
+        <Route path="/task03/react">
+          <Shop products={getItemsFromCategory("React")} />
+        </Route>
+
+        <Route path="/404.html">
+          <NotFound />
+        </Route>
+
+        <Route>
+          <Redirect to="/404.html" />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default Task03;
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { Redirect, useParams } from "react-router-dom";
 
-const Product = ({name, description, price, link = null}) => {
+import products from "./../products.json";
 
-    const productTitle = link === null ? name : <a href={ link }>{name}</a>;
-    
+const Product = (props) => {
+  const { alias } = useParams();
+
+  if (alias === undefined) {
     return (
-        <article>
-            <h3>{productTitle}</h3>
-            <h4>{price}</h4>
-            <p>{description}</p>
-        </article>
+      <article>
+        <h3>{props.name}</h3>
+        <p>ID: {props.id}</p>
+        <p>{props.category}</p>
+        <h4>{props.price} zł</h4>
+        <p>{props.description}</p>
+      </article>
     );
-}
+  }
+
+  const [product = null] = products.filter(({ id, name }) => {
+    return (
+      id === parseInt(id) && name.replace(": ", "+").toLowerCase() === alias
+    );
+  });
+
+  if (product === null) {
+    return <Redirect to="/404.html" />;
+  }
+
+  return (
+    <article>
+      <h3>{product.name}</h3>
+      <p>ID: {product.id}</p>
+      <p>{product.category}</p>
+      <h4>{product.price} zł</h4>
+      <p>{product.description}</p>
+    </article>
+  );
+};
 
 export default Product;
