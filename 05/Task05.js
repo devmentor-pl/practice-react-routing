56cc7f0997c4486be0bb7f6db6f760d295851d41
Andrzej <andis_as@wp.pl>
2022-09-03 18:01:13 +0200

    zad 5 - phrase from input and URL filter products

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Shop from '../src/components/Shop';
 import products from './../src/products.json';
-import {Link, Route, useParams} from 'react-router-dom'
+import { Link, Route, useParams } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
 
+let newProduct = products
+
 const Art = () => {
-    const {search} = useParams()
+    const { search } = useParams()
+    // newProduct = products.filter(product => product.name.includes(search))
+    newProduct = products.filter(product => {
+        return product.name.toLowerCase().includes(search.toLowerCase()) ||
+            product.description.toLowerCase().includes(search.toLowerCase())
+    })
+    console.log( newProduct )
     return (
         <h2>Hello Art {search} </h2>
     )
@@ -15,27 +23,22 @@ const Task05 = () => {
     const history = useHistory()
     const [search, setSearch] = React.useState('')
     const handleInput = e => {
-        const value = e.target.value 
+        const value = e.target.value
         setSearch(value)
-        history.push('/task05/'+value)
+        history.push('/task05/' + value)
     }
     return (
         <div>
             <h1>Task05</h1>
             <form>
-                <input type="text" value={search} onChange={handleInput}/>
+                <input type="text" value={search} onChange={handleInput} />
             </form>
             <br />
-            {
-                search && <Link to='/task05/ddd'>Products price-desc - Ceny malejąco</Link>
-            }
 
             <Route path={'/task05/:search'}>
                 <Art />
+                <Shop products={newProduct} />
             </Route>
-            <Shop products={products}/>
-            
-
 
         </div>
     );
