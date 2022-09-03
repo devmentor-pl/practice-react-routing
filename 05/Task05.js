847df5f42b8698ecf9a96e24ba46d543581d9513
Andrzej <andis_as@wp.pl>
2022-09-03 20:06:35 +0200

    zad 5 - input & URL for min max search

--- 05/Task05.js
+++ 05/Task05.js
@@ -7,35 +7,70 @@ import { useHistory } from 'react-router-dom'
 let newProduct = products
 
 const Art = () => {
-    const { search } = useParams()
-    // newProduct = products.filter(product => product.name.includes(search))
+    const { min, max, search } = useParams()
+    console.log(min)
+    console.log(max)
+    console.log(search)
+    let minCut = min.substring(1, min.length - 1)
+    let maxCut = max.substring(1, max.length - 1)
+    let searchCut = search.substring(1, search.length - 1)
+    console.log(minCut)
+    console.log(maxCut)
+    console.log(searchCut)
     newProduct = products.filter(product => {
-        return product.name.toLowerCase().includes(search.toLowerCase()) ||
-            product.description.toLowerCase().includes(search.toLowerCase())
+        return product.name.toLowerCase().includes(searchCut.toLowerCase()) ||
+            product.description.toLowerCase().includes(searchCut.toLowerCase())
     })
-    console.log( newProduct )
+    // console.log( newProduct )
     return (
-        <h2>Hello Art {search} </h2>
+        <div>
+            <h2> Art Search </h2>
+            <div> {min},{max}-{searchCut}</div>
+        </div>
     )
 }
 
 const Task05 = () => {
     const history = useHistory()
     const [search, setSearch] = React.useState('')
+    const [min, setMin] = React.useState('')
+    const [max, setMax] = React.useState('')
+
     const handleInput = e => {
         const value = e.target.value
-        setSearch(value)
-        history.push('/task05/' + value)
+        const name = e.target.name
+
+        if (name === 'min') {
+            setMin(value)
+            history.push('/task05/[' + value + '],[' + max + ']-[' + search + ']')
+        }
+        if (name === 'max') {
+            setMax(value)
+            history.push('/task05/[' + min + '],[' + value + ']-[' + search + ']')
+        }
+        if (name === 'search') {
+            setSearch(value)
+            history.push('/task05/[' + min + '],[' + min + ']-[' + value + ']')
+        }
     }
+
     return (
         <div>
             <h1>Task05</h1>
             <form>
-                <input type="text" value={search} onChange={handleInput} />
+                <label htmlFor="min">min
+                    <input type="t ext" name='min' value={min} onChange={handleInput} /><br />
+                </label>
+                <label htmlFor="max">max
+                    <input type="text" name='max' value={max} onChange={handleInput} /><br />
+                </label>
+                <label htmlFor="search">search
+                    <input type="text" name='search' value={search} onChange={handleInput} /><br />
+                </label>
             </form>
             <br />
 
-            <Route path={'/task05/:search'}>
+            <Route path={'/task05/:min,:max-:search'}>
                 <Art />
                 <Shop products={newProduct} />
             </Route>
