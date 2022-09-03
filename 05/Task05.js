7cc4671f5340d829452ff0f88940d65de33ad6cb
Andrzej <andis_as@wp.pl>
2022-09-03 21:26:40 +0200

    zad 5

--- 05/Task05.js
+++ 05/Task05.js
@@ -4,28 +4,33 @@ import products from './../src/products.json';
 import { Link, Route, useParams } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
 
-let newProduct = products
+let newProducts = products
 
 const Art = () => {
     const { min, max, search } = useParams()
-    console.log(min)
-    console.log(max)
-    console.log(search)
+    // console.log(min)
+    // console.log(max)
+    // console.log(search)
     let minCut = min.substring(1, min.length - 1)
     let maxCut = max.substring(1, max.length - 1)
     let searchCut = search.substring(1, search.length - 1)
-    console.log(minCut)
-    console.log(maxCut)
-    console.log(searchCut)
-    newProduct = products.filter(product => {
-        return product.name.toLowerCase().includes(searchCut.toLowerCase()) ||
+    // console.log(minCut)
+    // console.log(maxCut)
+    // console.log(searchCut)
+    newProducts = products.filter(product => {
+        return (
+            product.price >= parseInt(minCut) && 
+            product.price <= parseInt(maxCut) &&
+            product.name.toLowerCase().includes(searchCut.toLowerCase()) ||
             product.description.toLowerCase().includes(searchCut.toLowerCase())
+        )
     })
-    // console.log( newProduct )
+    console.log( newProducts )
     return (
         <div>
             <h2> Art Search </h2>
-            <div> {min},{max}-{searchCut}</div>
+            <div> {minCut},{maxCut}-{searchCut}</div>
+            <div> {min},{max}-{search}</div>
         </div>
     )
 }
@@ -50,7 +55,7 @@ const Task05 = () => {
         }
         if (name === 'search') {
             setSearch(value)
-            history.push('/task05/[' + min + '],[' + min + ']-[' + value + ']')
+            history.push('/task05/[' + min + '],[' + max + ']-[' + value + ']')
         }
     }
 
@@ -59,20 +64,29 @@ const Task05 = () => {
             <h1>Task05</h1>
             <form>
                 <label htmlFor="min">min
-                    <input type="t ext" name='min' value={min} onChange={handleInput} /><br />
+                    <input type="number" name='min'  
+                        value={min} 
+                        onChange={handleInput} 
+                    /><br />
                 </label>
                 <label htmlFor="max">max
-                    <input type="text" name='max' value={max} onChange={handleInput} /><br />
+                    <input type="number" name='max' 
+                        value={max} 
+                        onChange={handleInput} 
+                    /><br />
                 </label>
                 <label htmlFor="search">search
-                    <input type="text" name='search' value={search} onChange={handleInput} /><br />
+                    <input type="text" name='search' 
+                        value={search} 
+                        onChange={handleInput}
+                    /><br />
                 </label>
             </form>
             <br />
 
             <Route path={'/task05/:min,:max-:search'}>
                 <Art />
-                <Shop products={newProduct} />
+                <Shop products={newProducts} />
             </Route>
 
         </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Product from './Product';
 
 const Shop = ({ products }) => {
+    console.log( products )
     return (
         <section>
             {
