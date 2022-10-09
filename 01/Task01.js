c0ee5fa99e0b5d6d169686e6cf9562c785a4ba3d
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-10-09 13:02:48 +0200

    Finish 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import {Route, NavLink} from 'react-router-dom';
+
+import Home from './../src/components/Home';
+import Contact from './../src/components/Contact';
+
 const Task01 = () => {
     return (
-        <h1>Task01</h1>
+        <>
+            <h1>Task 01</h1>
+            <ul>
+                <li><NavLink activeStyle={{border: '1px solid red'}} to="/task01/home">Home</NavLink></li>
+                <li><NavLink to="/task01/contact">Contact</NavLink></li>
+            </ul>
+
+            <Route path="/task01/home" component={Home}/>
+            <Route path="/task01/contact">
+                <Contact />
+            </Route>
+        </>
     );
 }
 
