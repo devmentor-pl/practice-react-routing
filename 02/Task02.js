e4ca9c754ffee0b699c7e1aef5319f3dd16fdab6
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-10-09 17:55:42 +0200

    task 2 done

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Product from '../src/components/Product';
+import products from '../src/products.json'
 
 const Task02 = () => {
     return (
-        <h1>Task02</h1>
+        <>
+            <h1>Task02</h1>
+            {products.map((product) => {
+                return (<>
+                    <li><Link to={`/task02/product-${product.id}`}>{product.name}</Link></li>
+                    <Route key={product.id} path={`/task02/product-${product.id}`}>
+                        <Product name={product.name} description={product.description} price={product.price} />
+                    </Route>
+                </>)
+
+
+            })}
+        </>
+
     );
 }
 
