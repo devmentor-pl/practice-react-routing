3ad9bf363323e5e629c54c0455a10127423c0e85
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-10-09 13:22:45 +0200

    Finish 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
+import {Route, useParams, Redirect} from 'react-router-dom';
+
+import products from './../src/products.json';
+
+
+const Product = ({name, price, category, description}) => {
+    return (
+        <article>
+            <h1>{name}</h1>
+            <h2>{price}</h2>
+            <h3>{category}</h3>
+            <p>{description}</p>
+        </article>
+    );
+}
+
+const ProductPage = () => {
+    const {id} = useParams();
+    const [product = null] = products.filter(p => p.id === Number(id));
+
+    if(product === null) {
+        return <Redirect to="/404" />
+    }
+
+    return <Product {...product} />
+}
+
 const Task02 = () => {
     return (
-        <h1>Task02</h1>
+        <>
+            <h1>Task02</h1>
+            <Route path="/task02/product-:id">
+                <ProductPage/>
+            </Route>
+        </>
+
     );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <h2>Brak strony!</h2>;
+
+export default NotFound;
