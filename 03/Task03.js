f2b5acba0da4a94b125cce332801c93e9b35cc6d
Mateusz Suplewski <matx3582@gmail.com>
2022-10-12 13:30:02 +0200

    task-3 Done

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,12 +1,35 @@
-import React from 'react';
-
-import products from './../src/products.json';
+import React from "react";
+import { Route, NavLink } from "react-router-dom";
+import products from "./../src/products.json";
+import Shop from "../src/components/Shop";
 
 const Task03 = () => {
-    return (
-        <h1>Task03</h1>
-    );
-}
+  return (
+    <>
+      <h1>Task03</h1>
+      <ul>
+        <li>
+          <NavLink
+            activeStyle={{ borderRight: "5px solid green" }}
+            to={"/task03/javascript"}
+          >
+            javascript
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            activeStyle={{ borderRight: "5px solid green" }}
+            to={"/task03/react"}
+          >
+            react
+          </NavLink>
+        </li>
+      </ul>
+      <Route path="/task03/:category">
+        <Shop products={products} />
+      </Route>
+    </>
+  );
+};
 
 export default Task03;
-
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import Product from './Product';
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { Product } from "./Product";
 
-const Shop = (products) => {
-    return (
-        <section>
-            { products.map(p => <Product {...p} />)}
-        </section>
-    );
-}
+const Shop = ({ products }) => {
+  const { category } = useParams();
+  const productsInCategory = products.filter(
+    (product) => category === product.category.toLowerCase()
+  );
+
+  if (productsInCategory.length === 0) return <Redirect to={"/404"} />;
+
+  return (
+    <section>
+      {productsInCategory.map((product) => (
+        <Product
+          key={product.id}
+          {...product}
+          link={`/task03/${product.category}`}
+        />
+      ))}
+    </section>
+  );
+};
 
 export default Shop;
