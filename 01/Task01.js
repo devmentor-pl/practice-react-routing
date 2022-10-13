f5ae7cdd93f05f22a55ef0fc672300d36c157c0f
Camila Montes Celinski <camicelinski@gmail.com>
2022-10-13 22:41:21 +0200

    Finish task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import {Route, NavLink} from 'react-router-dom'
+
+import Home from '../src/components/Home'
+import Contact from '../src/components/Contact'
 
 const Task01 = () => {
+    const activeStyle = { backgroundColor: '#bbb', borderRadius: '5px', }
+    const activeClass = 'active';
+
     return (
-        <h1>Task01</h1>
+        <>
+            <h1>Task01</h1>
+            <ul>
+                <li>
+                    <NavLink 
+                        activeStyle={activeStyle}
+                        to='/task01/home'>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        activeClassName={activeClass}
+                        to='/task01/contact'>
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+            <Route path='/task01/home' component={Home}/>
+            <Route path='/task01/contact' component={Contact}/>
+        </>
     );
 }
 
 export default Task01;
-
