9fc1538b13cdf48097dd1388dd26019a6b459d0d
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-10-31 15:26:18 +0100

    Finish 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+import {Route, useParams, Redirect} from 'react-router-dom';
+
+import products from './../src/products.json';
+
+import Product from '../src/components/Product';
+
+const ProductPage = () => {
+    const {id} = useParams();
+    const [product = null] = products.filter(p => p.id === Number(id));
+
+    if(product === null) {
+        return <Redirect to="/404" />
+    }
+
+    return <Product {...product} />
+}
+
 const Task02 = () => {
     return (
-        <h1>Task02</h1>
+        <>
+            <h1>Task02</h1>
+            <Route path="/task02/product-:id">
+                <ProductPage/>
+            </Route>
+        </>
     );
 }
 
