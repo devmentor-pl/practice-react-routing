98532a5c840793d71fa0efd8ed0a873d3264d6a6
Dawid Drag <dede7d@wp.pl>
2023-01-30 18:55:55 +0100

    task01 done

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Contact from "../src/components/Contact";
+import Home from "../src/components/Home";
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+
+
+  return (
+    <Router>
+      <h1>Task01</h1>
+      <Route exact path="/task01/home" component={Home}/>
+      <Route path="/task01/contact" component={Contact}/>
+    </Router>
+  );
+};
 
 export default Task01;
-
