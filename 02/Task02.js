bc7d51de8a78fa120a57c9b52008d10ffc15972f
Dawid Drag <dede7d@wp.pl>
2023-01-30 20:23:33 +0100

    task02 done

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
+import products from "../src/products.json";
 
 const Task02 = () => {
+
+  const ProductComponent = () => {
+    const { id } = useParams();
+    const [product] = products.filter((prod) => prod.id === parseInt(id));
+ 
     return (
-        <h1>Task02</h1>
+      <section>
+        <p>{product.name}</p>
+      </section>
     );
-}
+
+
+  };
+  return (
+    <Router>
+      <h1>Task02</h1>
+      <Route
+        exact
+        path="/task02/product-:id"
+        component={ProductComponent}
+      ></Route>
+    </Router>
+  );
+};
 
 export default Task02;
-
