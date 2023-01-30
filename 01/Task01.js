a760ac28af5f3d40c703df70062861630ae32092
Katarzyna Panz <katarzyna.panz@gmail.com>
2023-01-30 14:04:40 +0100

    Add solution to exercise 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Home from "../src/components/Home";
+import Contact from "../src/components/Contact";
+import { Route, Link } from "react-router-dom";
 
 const Task01 = () => {
-    return (
+  const nav = (
+    <ul>
+      <li>
+        <Link to="/task01/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/task01/contact">Contact</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <>
+      <nav>
         <h1>Task01</h1>
-    );
-}
+        {nav}
+      </nav>
+      <Route path="/task01/home">
+        <Home />
+      </Route>
+      <Route path="/task01/contact">
+        <Contact />
+      </Route>
+    </>
+  );
+};
 
 export default Task01;
 
