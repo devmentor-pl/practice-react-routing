cef030871a1f0b3e9a733c4a54015c9c7ffc1ac7
Katarzyna Panz <katarzyna.panz@gmail.com>
2023-02-01 14:54:57 +0100

    Add form to exercise 05

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Task05 = () => {
-    return (
-        <h1>Task05</h1>
-    );
-}
+  const [form, setForm] = useState({
+    minPrice: 0,
+    maxPrice: 0,
+  });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(form.minPrice, form.maxPrice);
+  };
+  const inputChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  return (
+    <>
+      <h1>Task05</h1>
+      <form onSubmit={submitHandler}>
+        <label>
+          podaj cenę min.:
+          <input name="minPrice" value={form.minPrice} onChange={inputChange} />
+        </label>
+        <label>
+          podaj cenę max.:
+          <input name="maxPrice" value={form.maxPrice} onChange={inputChange} />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
 
 export default Task05;
-
