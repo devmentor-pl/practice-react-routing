a5f4d30e68a72c334c7602ec8c6a1dd7d7a43fbd
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-21 12:22:45 +0100

    Add navigation and routes

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Home from '../src/components/Home';
+import Contact from '../src/components/Contact';
+import { NavLink, Route } from 'react-router-dom';
 
 const Task01 = () => {
+    const nav = <ul>
+        <li>
+            <NavLink to="/task01/home">Home</NavLink>
+        </li>
+        <li>
+            <NavLink to="/task01/contact">Contact</NavLink>
+        </li>
+    </ul>
     return (
-        <h1>Task01</h1>
+        <>
+            <h1>Task01</h1>
+            {nav}
+            <Route path="/task01/home">
+                <Home></Home>
+            </Route>
+            <Route path="/task01/contact">
+                <Contact></Contact>
+            </Route>
+        </>
+        
     );
 }
 
