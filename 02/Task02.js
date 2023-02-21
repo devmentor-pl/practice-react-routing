bccadd7cdfcf69b3c032cc4dcda3382e766a8e46
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-21 12:57:57 +0100

    Add navigation and routes

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import products from "../src/products.json"
+import Product from '../src/components/Product';
 
 const Task02 = () => {
+    const nav = <ul>
+        {products.map(item => <li key={item.id}><Link to={`/task02/product-${item.id}`}>{item.name}</Link></li>)}
+    </ul> 
+    const routes = products.map(item => <Route path={`/task02/product-${item.id}`}>
+        <Product description={item.description} 
+                price={item.price} 
+                name={item.name} 
+                category={item.category}>    
+        </Product>
+    </Route>)
     return (
-        <h1>Task02</h1>
+        <>
+            <h1>Task02</h1>
+            {nav}
+            <Switch>
+                {routes}
+            </Switch>
+        </>
+        
     );
 }
 
