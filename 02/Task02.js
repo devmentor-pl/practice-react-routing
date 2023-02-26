14a5dfcd897618ddd561d1aebcda01c65e94f144
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-26 14:30:15 +0100

    Change task2 & product component

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import products from "../src/products.json"
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import Product from '../src/components/Product';
+import NotFound from '../src/views/NotFound';
 
 const Task02 = () => {
-    const nav = <ul>
-        {products.map(item => <li key={item.id}><Link to={`/task02/product-${item.id}`}>{item.name}</Link></li>)}
-    </ul> 
-    const routes = products.map(item => <Route path={`/task02/product-${item.id}`}>
-        <Product description={item.description} 
-                price={item.price} 
-                name={item.name} 
-                category={item.category}>    
-        </Product>
-    </Route>)
+
     return (
         <>
             <h1>Task02</h1>
-            {nav}
-            <Switch>
-                {routes}
-            </Switch>
+            <Route path="/task02"/>
+            <Route path="/task02/product-:id">
+                <Product/>
+            </Route>
+            <Route path="/task02/404.html">
+                <NotFound/>
+            </Route>
+            {/* <Route>
+                <Redirect to="/404.html"/>
+            </Route> */}
+ 
         </>
         
     );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import products from "../products.json"
+import { useParams } from "react-router-dom";
+import NotFound from "../views/NotFound";
 
-const Product = (props) => {
-    const {price, description, name, category} = props
+const Product = () => {
+    const {id} = useParams()
+    console.log(id)
+    const [product = null] = products.filter(item => item.id === parseInt(id))
+    if(product === null){
+       return <Redirect to="/task02/404.html"/>
+    } 
+    const {name, category, description, price } = product
     return (
         <div>
             <h2>{name}</h2>
