9269c95c703bdbc5f14876f97f29b7d0664a5cf3
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-02-27 20:41:05 +0100

    Fix task2

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route} from 'react-router-dom';
 import Product from '../src/components/Product';
-import NotFound from '../src/views/NotFound';
-
 const Task02 = () => {
 
     return (
         <>
             <h1>Task02</h1>
-            <Route path="/task02"/>
             <Route path="/task02/product-:id">
                 <Product/>
             </Route>
+            
             <Route path="/task02/404.html">
-                <NotFound/>
+                <p>Nie znaleziono produktu</p>
             </Route>
-            {/* <Route>
-                <Redirect to="/404.html"/>
-            </Route> */}
- 
         </>
         
     );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,25 +1,37 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
 import products from "../products.json"
-import { useParams } from "react-router-dom";
-import NotFound from "../views/NotFound";
 
-const Product = () => {
-    const {id} = useParams()
-    console.log(id)
-    const [product = null] = products.filter(item => item.id === parseInt(id))
-    if(product === null){
-       return <Redirect to="/task02/404.html"/>
-    } 
-    const {name, category, description, price } = product
-    return (
-        <div>
+const Product = (props) => {
+    const { id } = useParams()
+    console.log(props,'props')
+    
+    const [product = null] = products.filter(p => p.id === parseInt(id))
+
+    if(product === null && Object.keys(props).length === 0 ){
+        return <Redirect to="/task02/404.html"/>
+    }
+    const renderProducts = () => {
+        const {name, category, price, description} = product
+        return (
+            <div data-id={id}>
             <h2>{name}</h2>
-            <p>Category:{category}</p>
+            <p>{category}</p>
             <p>{description}</p>
-            <p>Price: {price}$</p>
+            <p>{price}$</p>
         </div>
+        )
+    }
+    return (
+        Object.keys(props).length > 0
+        ? <div data-id={props.id}>
+            <h2><Link to={`/task02/product-${props.id}`}>{props.name}</Link></h2>
+            <p>{props.category}</p>
+            <p>{props.description}</p>
+            <p>{props.price}$</p>
+        </div>
+        :renderProducts()
     )
-}
 
+    }
 export default Product
