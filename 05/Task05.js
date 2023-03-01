5ae5b5b7384ef826640a930b77cfbdff83065f7f
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-03-01 23:15:44 +0100

    Fix task5

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,52 +1,50 @@
 import React, {useState, useEffect} from 'react';
 import Shop from '../src/components/Shop';
-import { useParams, Switch, Route, useHistory } from 'react-router-dom';
+import { useParams, Switch, Route, useHistory, Redirect } from 'react-router-dom';
 import products from "../src/products.json"
-
+import ShopWithSortParams from '../src/components/ShopWithSortParams';
 
 
 const Task05 = () => {
     const history = useHistory();
+
     const [state, setState] = useState({
         minPrice: '',
         maxPrice: '',
         searchTerm: '',
     })
-    const handleSubmit = event => {
-        event.preventDefault()
-        const {minPrice, maxPrice, searchTerm} = event.target.elements
-        
+
+    const handleChange = event => {
+        const {name, value} = event.target;
         setState({
-            minPrice: minPrice.value,
-            maxPrice: maxPrice.value,
-            searchTerm: searchTerm.value,
+            ...state,
+            [name]: value
         })
-        history.push(
-            `/task05/${minPrice.value}-${maxPrice.value}-${searchTerm.value}`
-            );
-        }
-
-    const sortProducts = () => {
-       console.log(state.maxPrice, state.maxPrice, state.searchTerm, ' state')
-        return products.filter(item => item.price >= state.minPrice 
-            && item.price <= state.maxPrice 
-            && item.name.includes(state.searchTerm))
-
     }
+    useEffect(()=>{
+        const {minPrice, maxPrice, searchTerm} = state
+        if(minPrice !== '' || maxPrice !== '' || searchTerm !== ''){
+            history.push(
+                `/task05/${minPrice}-${maxPrice}-${searchTerm}`
+            )
+        }else{
+            history.replace('/task05')
+        }
+    }, [state])
+        
     return (
         <>
             <h1>Task05</h1>
-            <form onSubmit={handleSubmit}>
-                <input name="minPrice" type="number" placeholder='cena minimalna'></input>
-                <input name="maxPrice" type="number" placeholder='cena maksymalna'></input>
-                <input name="searchTerm" type="text" placeholder='fraza'></input>
-                <input type="submit" value="filter"/>
+            <form>
+                <input onChange={handleChange} value={state.minPrice} name="minPrice" type="number" placeholder='cena minimalna'></input>
+                <input onChange={handleChange} value={state.maxPrice} name="maxPrice" type="number" placeholder='cena maksymalna'></input>
+                <input onChange={handleChange} value={state.searchTerm} name="searchTerm" type="text" placeholder='fraza'></input>
             </form>
             <Route exact path={`/task05`}>
                 <Shop products={products}></Shop>
              </Route>
-             <Route exact path='/task05/:min-:max-:term'>
-                 <Shop products={sortProducts()}/>
+             <Route path='/task05/:min?-:max?-:term?'>
+                 <ShopWithSortParams products={products}/>
              </Route>
         </>
     );
