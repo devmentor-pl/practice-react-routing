3bca518f0ab282a0fcdca7d19db5a4a7e7aa9df7
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-03-01 23:14:45 +0100

    Fix task3

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import Shop from '../src/components/Shop';
 import products from './../src/products.json';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, useLocation } from 'react-router-dom';
+import ShopWithCategory from '../src/components/ShopWithCategory';
 
 const Task03 = () => {
-    const filterProducts = (category) => {
-       return products.filter(p => p.category === category)
-    }
-    return (
-        <>
+
+    return (<>
             <h1>Task03</h1>
-     
+            <Switch>
                 <Route exact path={`/task03`}>
                     <Shop products={products}></Shop>
                 </Route>
-                <Route path={`/task03/javascript`}>
-                    <Shop products={filterProducts('JavaScript')}></Shop>
+                <Route path={`/task03/:category`} >
+                    <ShopWithCategory products={products}/>
                 </Route>
-                <Route path={`/task03/react`}>
-                    <Shop products={filterProducts('React')}></Shop>
+                <Route path={`/task03/404.html`}>
+                    <p>Brak produktów w wybranej kategorii</p>
                 </Route>
-      
-        </>
-    );
+            </Switch>
+    </>)
 }
 
 export default Task03;
