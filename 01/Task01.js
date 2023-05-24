b38f3623a6db49e4d28a179c907208caba56bb95
Michal <maxpoz3212@gmail.com>
2023-05-24 13:48:42 +0200

    Add task01 solution

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "../src/components/Home";
+import Contact from "../src/components/Contact";
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+  return (
+    <Router>
+      <h1>Task01</h1>
+      <Switch>
+        <Route path="/task01/home">
+          <Home />
+        </Route>
+        <Route path="/task01/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Task01;
 
