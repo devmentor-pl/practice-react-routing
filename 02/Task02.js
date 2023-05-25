9fdabe8a3dd37da024c639d723c34d05e3be8e6f
Michal <maxpoz3212@gmail.com>
2023-05-25 10:38:27 +0200

    Add fix in task02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Product from "../src/components/Product";
 
@@ -7,13 +7,9 @@ const Task02 = () => {
   return (
     <>
       <h1>Task02</h1>
-      <Router>
-        <Switch>
-          <Route path="/task02/product-:id">
-            <Product />
-          </Route>
-        </Switch>
-      </Router>
+      <Route path="/task02/product-:id">
+        <Product />
+      </Route>
     </>
   );
 };
