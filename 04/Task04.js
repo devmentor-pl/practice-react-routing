54cd145f779775cb3b965d1f67f8fcbdecd83b7b
Michal <maxpoz3212@gmail.com>
2023-05-25 14:50:38 +0200

    Add task04 solution

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useHistory, Switch, Route } from "react-router-dom";
+
+import products from "../src/products.json";
+import Shop from "../src/components/Shop";
 
 const Task04 = () => {
-    return (
-        <h1>Task04</h1>
-    );
-}
+  const history = useHistory();
+  const handleChange = (event) => {
+    history.push(`/task04${event.target.value}`);
+  };
+  const sortedProducts = (order) => {
+    if (order === "/task04/price-desc") {
+      return products.sort((a, b) => b.price - a.price);
+    } else if (order === "/task04/price-asc") {
+      return products.sort((a, b) => a.price - b.price);
+    }
+  };
+
+  console.log(history.location.pathname);
+
+  return (
+    <>
+      <h1>Task04</h1>
+      <select onChange={handleChange}>
+        <option value="">Sortuj</option>
+        <option value="/price-desc">Cena malejąco</option>
+        <option value="/price-asc">Cena rosnąco</option>
+      </select>
+      <Switch>
+        <Route path="/task04">
+          <Shop products={products} />
+        </Route>
+        <Route path="/task04/:order">
+          <Shop products={sortedProducts(history.location.pathname)} />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default Task04;
 
