1f210cb5ae5227c14f7e9a9a45aef5152f9e9fbf
Michal <maxpoz3212@gmail.com>
2023-05-25 10:14:26 +0200

    Add fix in task01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Home from "../src/components/Home";
 import Contact from "../src/components/Contact";
 
 const Task01 = () => {
   return (
-    <Router>
+    <>
       <h1>Task01</h1>
-      <Switch>
         <Route path="/task01/home">
           <Home />
         </Route>
         <Route path="/task01/contact">
           <Contact />
         </Route>
-      </Switch>
-    </Router>
+    </>
   );
 };
 
