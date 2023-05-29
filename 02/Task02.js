0e39272056e2ae9bfa63a88190171ddfd650a47b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-05-29 14:35:24 +0200

    Finish task 2

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Product from '../src/components/Product';
 
 const Task02 = () => {
-    return (
-        <h1>Task02</h1>
-    );
-}
+	return (
+		<>
+			<h1>Task02</h1>
+			<Route path='/task02/product-:id'>
+				<Product />
+			</Route>
+		</>
+	);
+};
 
 export default Task02;
 
---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import products from '../products.json';
+
+const Product = () => {
+	const { id } = useParams();
+	const [product = null] = products.filter((item) => item.id === parseInt(id));
+
+	if (product === null) {
+		return <Redirect to='/404.html' />;
+	}
+
+	const { name, description, price, category } = product;
+
+	return (
+		<div>
+			<h2>{name}</h2>
+			<p>Opis: {description}</p>
+			<p>Cena: {price}zł</p>
+			<p>Kategoria: {category}</p>
+		</div>
+	);
+};
+
+export default Product;
