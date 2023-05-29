b190a0ddd14551d852528a3e494f051d1e525f13
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-05-29 20:17:24 +0200

    finish task 5

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Shop from '../src/components/Shop';
+import ShopFiltered from '../src/components/ShopFiltered';
+import products from '../src/products.json';
+import { Route, useHistory } from 'react-router-dom';
+
+const initialState = {
+	minPrice: '',
+	maxPrice: '',
+	searchPhrase: '',
+};
 
 const Task05 = () => {
-    return (
-        <h1>Task05</h1>
-    );
-}
+	const [form, setForm] = useState(initialState);
+	const history = useHistory();
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setForm({ ...form, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const { minPrice, maxPrice, searchPhrase } = form;
+		if (minPrice !== '' || maxPrice !== '' || searchPhrase !== '') {
+			history.push(`/task05/${minPrice},${maxPrice}-${searchPhrase}`);
+		} else {
+			history.replace('/task05');
+		}
+	};
+
+	return (
+		<>
+			<h1>Task05</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='number'
+					value={form.minPrice}
+					name='minPrice'
+					onChange={handleChange}
+					placeholder='cena minimalna'
+				/>
+				<input
+					type='number'
+					value={form.maxPrice}
+					name='maxPrice'
+					onChange={handleChange}
+					placeholder='cena maksymalna'
+				/>
+				<input
+					type='text'
+					value={form.searchPhrase}
+					name='searchPhrase'
+					onChange={handleChange}
+					placeholder='szukaj po nazwie'
+				/>
+				<button>filtruj</button>
+			</form>
+			<Route exact path='/task05'>
+				<Shop products={products} />
+			</Route>
+			<Route path='/task05/:minPrice?,:maxPrice?-:searchPhrase?'>
+				<ShopFiltered products={products} />
+			</Route>
+		</>
+	);
+};
 
 export default Task05;
-
---
+++ src/components/ShopFiltered.js
@@ -0,0 +1,38 @@
+﻿import React from 'react';
+import Shop from './Shop';
+import { useParams } from 'react-router-dom';
+
+const ShopFiltered = ({ products }) => {
+	const { minPrice, maxPrice, searchPhrase = '' } = useParams();
+
+	const filterByMinPrice = (products) => {
+		return minPrice === undefined
+			? products
+			: products.filter((item) => item.price >= parseInt(minPrice));
+	};
+
+	const filterByMaxPrice = (products) => {
+		return maxPrice === undefined
+			? products
+			: products.filter((item) => item.price <= parseInt(maxPrice));
+	};
+
+	const filterBySearchPhrase = (products) => {
+		const lowerSearchPhrase = searchPhrase.toLowerCase();
+		return lowerSearchPhrase === ''
+			? products
+			: products.filter(
+					(item) =>
+						item.name.toLowerCase().includes(lowerSearchPhrase) ||
+						item.description.toLowerCase().includes(lowerSearchPhrase)
+			  );
+	};
+
+	const productsByMinPrice = filterByMinPrice(products);
+	const productsByMaxPrice = filterByMaxPrice(productsByMinPrice);
+	const productsBySearchPhrase = filterBySearchPhrase(productsByMaxPrice);
+
+	return <Shop products={productsBySearchPhrase} />;
+};
+
+export default ShopFiltered;
