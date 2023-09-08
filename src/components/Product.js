0fcbf193e41b49bcc67e4acab50c801b8c75ea52
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-09-08 16:10:22 +0200

    task 3 added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, useParams, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { Redirect, useParams, useLocation, Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Product = (product) => {
   const { category = '/', id = null} = useParams()
@@ -13,7 +13,7 @@ const Product = (product) => {
   if (Number(id) === product.id || (category === '/' && id === null) || category === product.category.toLowerCase()) {
     return (
       <div>
-      <p>product id: {product.id}</p>
+      <p>product id: { baseUrl === '/task02' ? product.id : <Link to={`task02/product-${product.id}`}>{product.id}</Link>}</p>
       <p>product name: {product.name}</p>
       <p>product price: {product.price}</p>
       <p>product description: {product.description}</p>
