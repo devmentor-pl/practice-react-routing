de2a106a85924b3a1f0ee0d4ab5b98539ca2cc61
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-09-18 14:24:29 +0200

    task-04 done

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Route, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+import Shop from '../src/components/Shop';
+import products from './../src/products.json';
 
 const Task04 = () => {
-    return (
-        <h1>Task04</h1>
-    );
-}
+	const history = useHistory();
+    
+	const handleSelect = (e) => {
+		history.push(e.target.value);
+	};
+
+	return (
+		<>
+			<h1>Task04</h1>
+			<label htmlFor='sort-price'>Sortowanie: </label>
+			<select
+				id='sort-price'
+				name='price'
+				onChange={handleSelect}>
+				<option value='/task04'>wybierz</option>
+				<option value='/task04/price-asc'>cena rosnąco</option>
+				<option value='/task04/price-desc'>cena malejąco</option>
+			</select>
+
+			<Route path='/task04/price-asc'>
+				<Shop products={[...products].sort((a, b) => a.price - b.price)} />
+			</Route>
+			<Route path='/task04/price-desc'>
+				<Shop products={[...products].sort((a, b) => b.price - a.price)} />
+			</Route>
+		</>
+	);
+};
 
 export default Task04;
-
