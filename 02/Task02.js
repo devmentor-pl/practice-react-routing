4f9402c9c096e85f9ce022aa75015fbb3e536a83
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-11-06 17:36:33 +0100

    Finish task 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom';
+import Product from '../src/components/Product'
+
 
 const Task02 = () => {
     return (
-        <h1>Task02</h1>
+        <Router>
+            <h1>Task02</h1>
+            <Route path='/task02/product-:id'>
+                <Product />
+            </Route>
+        </Router>
     );
 }
 
