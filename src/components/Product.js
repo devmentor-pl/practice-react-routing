155de8f4abbdfe6f04543ae7a941d16107990385
Marcin <xcinek1337@wp.pl>
2023-11-25 17:24:31 +0100

    03

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { useParams, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import products from '../products.json';
-
-const Product = () => {
-	const { id, alias } = useParams();
-	const [product = null] = products.filter((prod) => prod.id === parseInt(id));
-
-	if (product === null) {
-		return <Redirect to='/task02/404.html' />;
-	}
-	const { name, description, category, price } = product;
+const Product = ({ id, name, description, category, price }) => {
 	return (
-		<article
-			data-id={id}
-			data-alias={alias}
-		>
-			<h1>Produkt: {name}</h1>
-			<p>Kategoria: {category}</p>
-			<p>Opis: {description}</p>
-			<p>Cena: {price}</p>
-		</article>
+		<>
+			<ul>
+				<li>
+					<Link to={`/task02/product-${id}`}>{name}</Link>
+				</li>
+			</ul>
+		</>
 	);
 };
+
 export default Product;
