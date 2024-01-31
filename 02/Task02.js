6bfbc39e6c1bcf202080b36bfa994eb502e8b021
Maciej Necka <macieknecka@gmail.com>
2024-01-31 11:36:09 +0100

    Task 02 - Fixes

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Product from '../src/components/Product';
-import { Link } from 'react-router-dom';
 
 const Task02 = () => {
   return (
     <>
       <h1>Task02</h1>
-      <ul>
-        <li>
-          <Link to="/task02/product-1">Link to product 1</Link>
-        </li>
-        <li>
-          <Link to="/task02/product-2">Link to product 2</Link>
-        </li>
-        <li>
-          <Link to="/task02/product-3">Link to product 3</Link>
-        </li>
-        <li>
-          <Link to="/task02/product-4">Not found</Link>
-        </li>
-      </ul>
-      <Route path="/task02/product-:productId" component={Product} />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/task02/product-1">JavaScript: DOM</Link>
+          </li>
+          <li>
+            <Link to="/task02/product-2">JavaScript: Events</Link>
+          </li>
+          <li>
+            <Link to="/task02/product-3">React: Components</Link>
+          </li>
+          <li>
+            <Link to="/task02/product-4">React: Composition</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/task02/product-:id">
+          <Product />
+        </Route>
+      </Switch>
     </>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import products from '../products.json';
 
 const Product = () => {
-  const { productId } = useParams();
-
-  const products = [
-    {
-      id: 1,
-      name: 'Name 1',
-      description: 'Description for product 1',
-    },
-    {
-      id: 2,
-      name: 'Name 2',
-      description: 'Description for product 2',
-    },
-    {
-      id: 3,
-      name: 'Name 3',
-      description: 'Description for product 3',
-    },
-  ];
-
-  const product = products.find((product) => product.id === Number(productId));
+  const { id } = useParams();
+  const [product = null] = products.filter(
+    (item) => item.id === parseInt(id, 10)
+  );
 
   if (!product) {
     return <div>Product not found.</div>;
   }
+
+  const { name, description, category, price } = product;
+
   return (
     <div>
-      <h2>{product.name}</h2>
-      <p>ID: {product.id}</p>
-      <p>{product.description}</p>
+      <h2>{name}</h2>
+      <p>Category: {category}</p>
+      <p>Price: {price}</p>
+      <p>Description: {description}</p>
     </div>
   );
 };
