3b9f847bdc62c20fc16d3ef159d65561fc5608cd
Maciej Necka <macieknecka@gmail.com>
2024-01-31 16:11:49 +0100

    Task 05 - Done

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import products from '../src/products.json';
+import ProductFilterForm from '../src/components/ProductFilterForm';
+import Shop from '../src/components/Shop';
 
 const Task05 = () => {
-    return (
-        <h1>Task05</h1>
-    );
-}
+  const history = useHistory();
+  const location = useLocation();
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.hash.split('?')[1]);
+    const minPrice = params.get('minPrice');
+    const maxPrice = params.get('maxPrice');
+    const searchTerm = params.get('searchTerm');
+
+    const result = products.filter((product) => {
+      return (
+        (!minPrice || product.price >= minPrice) &&
+        (!maxPrice || product.price <= maxPrice) &&
+        (!searchTerm ||
+          product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          product.description.toLowerCase().includes(searchTerm.toLowerCase()))
+      );
+    });
+
+    setFilteredProducts(result);
+  }, [location.hash]);
+
+  const handleFilterChange = (filters) => {
+    const query = `#${
+      filters.searchTerm ? `/search/${filters.searchTerm}` : ''
+    }?minPrice=${filters.minPrice}&maxPrice=${filters.maxPrice}&searchTerm=${
+      filters.searchTerm
+    }`;
+    history.push(query);
+  };
+
+  return (
+    <div>
+      <h1>Task05</h1>
+      <ProductFilterForm onFilterChange={handleFilterChange} />
+      <Shop products={filteredProducts} />
+    </div>
+  );
+};
 
 export default Task05;
-
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Product from './Product';
 
 const Shop = ({ products }) => {
+  if (products.length === 0) {
+    return (
+      <section>
+        <p>No matching products found.</p>
+      </section>
+    );
+  }
   return (
     <section>
       {products.map((p) => (
