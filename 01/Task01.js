6ba944f0e9aec189ac015f3e56fd10bd04406cca
Kaczupinko <kaczupinko@gmail.com>
2024-02-19 17:22:04 +0000

    Finish 01 task

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../src/components/Home';
+import Contact from '../src/components/Contact';
+import { Link } from 'react-router-dom';
 
 const Task01 = () => {
     return (
-        <h1>Task01</h1>
+      <Router>
+        <div>
+          <h1>Task 01</h1>
+          <nav>
+            <Link to="/task01/home">Home</Link> | 
+            <Link to="/task01/contact">Contact</Link>
+          </nav>
+          <Switch>
+            <Route path="/task01/home" component={Home} />
+            <Route path="/task01/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
     );
 }
 
