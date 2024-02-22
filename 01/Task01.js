94ad64d25df13f3a664ab59359365a9bd541c7a3
Kaczupinko <kaczupinko@gmail.com>
2024-02-22 18:33:16 +0000

    Reafactor task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../src/components/Home';
-import Contact from '../src/components/Contact';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Home from "../src/components/Home";
+import Contact from "../src/components/Contact";
 
 const Task01 = () => {
     return (
-      <Router>
-        <div>
-          <h1>Task 01</h1>
-          <nav>
-            <Link to="/task01/home">Home</Link> | 
-            <Link to="/task01/contact">Contact</Link>
-          </nav>
-          <Switch>
-            <Route path="/task01/home" component={Home} />
-            <Route path="/task01/contact" component={Contact} />
-          </Switch>
-        </div>
-      </Router>
+        <>
+        <h1>Task01</h1>
+        <ul>
+            <li>
+            <Link to={"/task01/home"}>home</Link>
+            </li>
+            <li>
+            <Link to={"/task01/contact"}>contact</Link>
+            </li>
+        </ul>
+        <Route path="/task01/home" component={Home} />
+        <Route path="/task01/contact">
+            <Contact />
+        </Route>
+        </>
     );
-}
+};
 
 export default Task01;
-
