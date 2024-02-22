5ac511eeaf3ef16842ecf293094b7d9b58bade9c
Kaczupinko <kaczupinko@gmail.com>
2024-02-22 18:34:11 +0000

    Refactor task 03

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import products from './../src/products.json';
-import Shop from '../src/components/Shop';
+import React from "react";
+import { Route, NavLink } from "react-router-dom";
+import products from "./../src/products.json";
+import {Shop, CategoryPage} from "../src/components/Shop";
 
 const Task03 = () => {
     return (
-        <Router>
-            <div>
-                <h1>Task03</h1>
-                <nav>
-                    <Link to="/task03">Wszystkie Produkty</Link> | 
-                    <Link to="/task03/javascript">JavaScript</Link> | 
-                    <Link to="/task03/react">React</Link>
-                </nav>
-                <Switch>
-                    <Route exact path="/task03" render={() => <Shop products={products} />} />
-                    <Route path="/task03/:category" render={({ match }) => {
-                        const { category } = match.params;
-                        const filteredProducts = products.filter(p => p.category.toLowerCase() === category.toLowerCase());
-                        return <Shop products={filteredProducts} />;
-                    }} />
-                </Switch>
-            </div>
-        </Router>
+        <>
+        <h1>Task03</h1>
+        <ul>
+            <li>
+            <NavLink
+                activeStyle={{ borderRight: "5px solid green" }}
+                to={"/task03/javascript"}
+            >
+                javascript
+            </NavLink>
+            </li>
+            <li>
+            <NavLink
+                activeStyle={{ borderRight: "5px solid green" }}
+                to={"/task03/react"}
+            >
+                react
+            </NavLink>
+            </li>
+        </ul>
+
+        <Route exact path="/task03">
+            <Shop products={products} />
+        </Route>
+
+        <Route path="/task03/:category">
+            <CategoryPage/>
+        </Route>
+        </>
     );
-}
+};
 
 export default Task03;
-
