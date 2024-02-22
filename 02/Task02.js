26f790aa08d7b3589ce496127b034bf52eecc662
Kaczupinko <kaczupinko@gmail.com>
2024-02-22 18:34:02 +0000

    Refactor task 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,27 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
-import Product from '../src/components/Product'; 
+import React from "react";
+import { ProductPage } from "../src/components/Product";
+import { Route } from "react-router-dom";
 
-
-const Navigation = () => (
-    <nav>
-        <NavLink to="/task02/product-1">Produkt 1</NavLink> |
-        <NavLink to="/task02/product-2">Produkt 2</NavLink> |
-        <NavLink to="/task02/product-3">Produkt 3</NavLink> |
-        <NavLink to="/task02/product-4">Produkt 4</NavLink>
-    </nav>
-);
-  
 const Task02 = () => {
     return (
-        <Router>
-            <Navigation />
-            <Switch>
-                <Route exact path="/task02" component={() => <h1>Witaj w sklepie!</h1>} />
-                <Route path="/task02/product-:id" component={Product} />
-            </Switch>
-        </Router>
+        <>
+        <h1>Task02</h1>
+        <Route path="/task02/product-:id">
+            <ProductPage />
+        </Route>
+        </>
     );
-}
+};
 
 export default Task02;
