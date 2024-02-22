91c82f9afbe17e9be551a379c9fff76247474a0f
Kaczupinko <kaczupinko@gmail.com>
2024-02-22 18:32:56 +0000

    Refactor Shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import Product from './Product';
+import React from "react";
+import { Redirect, useParams, Route } from "react-router-dom";
+import { Product } from "./Product";
+import products from "../products.json";
 
-const Shop = ({products}) => {
+const Shop = ({ products }) => {
     return (
-        <section>
-            {products.map(product => (
-                <Product key={product.id} {...product} />
-            ))}
-        </section>
+      <section>
+        {products.map((product, index) => (
+          <Route
+            key={product.id}
+            exact
+            path={[
+              `/task03/${product.category.toLowerCase()}/${index + 1}`,
+              `/task03/${product.category.toLowerCase()}`,
+              `/task03`
+            ]}
+            render={() => (
+              <Product
+                {...product}
+                link={`/task03/${product.category.toLowerCase()}/${index + 1}`}
+              />
+            )}
+          />
+        ))}
+      </section>
     );
-}
+};
 
-export default Shop;
+const CategoryPage = () => {
+    const { category } = useParams();
+    const filteredProducts = products.filter(
+      (product) => product.category.toLowerCase() === category
+    );
+  
+    if (filteredProducts.length === 0) {
+        return <Redirect to="/404" />;
+    }
+  
+    return <Shop products={filteredProducts} />;
+};
+
+const SimpleShop = ({ products }) => {
+    return products.map((product) => (
+        <Product key={product.id} {...product} />
+    ));
+};
+
+export { Shop, CategoryPage, SimpleShop };
